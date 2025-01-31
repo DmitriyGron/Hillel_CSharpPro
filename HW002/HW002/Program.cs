aada5072ade01c4082334981324db95161a59a41
Гронь Дмитро <96023843+DmitriyGron@users.noreply.github.com>
2025-01-31 18:32:31 +0200

    new commit

--- HW002/HW002/Program.cs
+++ HW002/HW002/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace HW002
+﻿using System;
+
+namespace HW002
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("for second work");
         }
     }
 }
