43eb4c04add11e8221997c0d797866327296769f
Гронь Дмитро <96023843+DmitriyGron@users.noreply.github.com>
2025-01-31 02:19:23 +0200

    changetProgram

--- HW001/HW001/Program.cs
+++ HW001/HW001/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HW001
+﻿namespace HW001
 {
     internal class Program
     {
