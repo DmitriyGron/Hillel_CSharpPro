d6dd02184dbf8eb71afdf9fc47374da40d655c07
Гронь Дмитро <96023843+DmitriyGron@users.noreply.github.com>
2025-01-31 22:07:21 +0200

    finished work

--- HW001/HW001/Calculator.cs
+++ HW001/HW001/Calculator.cs
@@ -1,7 +1,29 @@
-﻿namespace HW001
+﻿using System;
+
+namespace HW001
 {
     public class Calculator
     {
-
+        public double Addition(double firstValue, double secondValue)
+        {
+            return firstValue + secondValue;
+        }
+        public double Subtraction(double firstValue, double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+        public double Multiplication(double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+        public double Division(double firstValue, double secondValue)
+        {
+            if (secondValue == 0)
+            {
+                Console.WriteLine("Error: Division by zero!");
+                return double.NaN; 
+            }
+            return firstValue / secondValue;
+        }
     }
 }
--- HW001/HW001/Program.cs
+++ HW001/HW001/Program.cs
@@ -7,6 +7,47 @@ namespace HW001
         static void Main(string[] args)
         {
             Calculator calculator = new Calculator();
+
+            while (true)
+            {
+                Console.Write("Enter first value: ");
+                double firstValue = double.Parse(Console.ReadLine());
+                Console.Write("Enter second value: ");
+                double secondValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("select operation: '+', '-', '*', '/'");
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine($"{firstValue} + {secondValue} = " + calculator.Addition(firstValue, secondValue));
+                        break;
+                    case "-":
+                        Console.WriteLine($"{firstValue} - {secondValue} = " + calculator.Subtraction(firstValue, secondValue));
+                        break;
+                    case "*":
+                        Console.WriteLine($"{firstValue} * {secondValue} = " + calculator.Multiplication(firstValue, secondValue));
+                        break;
+                    case "/":
+                        Console.WriteLine($"{firstValue} / {secondValue} = " + calculator.Division(firstValue, secondValue));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation");
+                        break;
+                }
+
+                Console.WriteLine("if you want to exit write the 'end'. ");
+                string exit = Console.ReadLine();
+                if (exit == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
     }
 }
