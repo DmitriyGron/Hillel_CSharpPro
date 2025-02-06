81757fa1d4c954f112cb9b23c2152872aedf3456
Гронь Дмитро <96023843+DmitriyGron@users.noreply.github.com>
2025-02-06 04:35:49 +0200

    HW002_3

---
+++ HW002/HW002_3/Decimal_Number.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HW002_3
+{
+    public struct Decimal_Number
+    {
+        public void toBinary(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            string binary = "";
+            while (number > 0)
+            {
+                binary = (number % 2) + binary;
+                number /= 2; 
+            }
+
+            Console.WriteLine(binary);
+        }
+
+        public void toOctal(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            string octal = "";
+            while (number > 0)
+            {
+                octal = (number % 8) + octal; 
+                number /= 8; 
+            }
+
+            Console.WriteLine(octal);
+        }
+
+        public void toHex(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            string hexadecimal = "";
+            string hexDigits = "0123456789ABCDEF"; 
+
+            while (number > 0)
+            {
+                hexadecimal = hexDigits[number % 16] + hexadecimal; 
+                number /= 16; 
+            }
+
+            Console.WriteLine(hexadecimal);
+        }
+    }
+}
---
+++ HW002/HW002_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW002_3
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            Decimal_Number decimalNumber = new Decimal_Number();
+
+            decimalNumber.toBinary(value);
+            decimalNumber.toOctal(value);
+            decimalNumber.toHex(value);
+
+        }
+    }
+}
