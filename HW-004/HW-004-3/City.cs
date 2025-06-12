04959c88d99a33658caaab54a29c97cadc86f8b2
DmitriyGron <unvmj5105775@gmail.com>
2025-06-12 16:43:11 +0300

    removed unnecessary usings

---
+++ HW-004/HW-004-3/City.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HW_004_3
+{
+    internal class City
+    {
+        public string NameOfCity { get; set; }
+        public decimal numberOfInhabitants { get; set; }
+
+        public City(string NameOfCity, decimal numberOfInhabitants)
+        {
+            this.NameOfCity = NameOfCity;
+            this.numberOfInhabitants = numberOfInhabitants;
+        }
+
+        public static City operator +(City a, decimal amount)
+        {
+            a.numberOfInhabitants += amount;
+            return a;
+        }
+
+        public static City operator -(City a, decimal amount)
+        {
+            a.numberOfInhabitants -= amount;
+            return a;
+        }
+
+        public static bool operator ==(City a, City b) => a.numberOfInhabitants == b.numberOfInhabitants;
+        public static bool operator !=(City a, City b) => a.numberOfInhabitants != b.numberOfInhabitants;
+        public static bool operator >(City a, City b) => a.numberOfInhabitants > b.numberOfInhabitants;
+        public static bool operator <(City a, City b) => a.numberOfInhabitants < b.numberOfInhabitants;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is City other)
+            {
+                return this.numberOfInhabitants == other.numberOfInhabitants;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return numberOfInhabitants.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"City: {NameOfCity}, Number of Inhabitants: {numberOfInhabitants}";
+        }
+    }
+}
--- HW-004/HW-004-3/Program.cs
+++ HW-004/HW-004-3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HW_004_3
 {
@@ -10,6 +6,28 @@ namespace HW_004_3
     {
         static void Main(string[] args)
         {
+            City city1 = new City("CityA", 1000000);
+            City city2 = new City("CityB", 2000000);
+
+            Console.WriteLine("City 1: " + city1.NameOfCity + ", Population: " + city1.numberOfInhabitants);
+            Console.WriteLine("City 2: " + city2.NameOfCity + ", Population: " + city2.numberOfInhabitants + "\n");
+
+            Console.WriteLine("Adding 280000 to City 1's population...");
+            city1 += 280000;
+            Console.WriteLine("City 1: " + city1.NameOfCity + ", Population: " + city1.numberOfInhabitants + "\n");
+
+            Console.WriteLine("Subtracting 460000 from City 2's population...");
+            city2 -= 460000;
+            Console.WriteLine("City 2: " + city2.NameOfCity + ", Population: " + city2.numberOfInhabitants + "\n");
+            Console.WriteLine("City 1 == City 2: " + (city1 == city2));
+            Console.WriteLine("City 1 != City 2: " + (city1 != city2));
+            Console.WriteLine("City 1 > City 2: " + (city1 > city2));
+            Console.WriteLine("City 1 < City 2: " + (city1 < city2));
+            Console.WriteLine("Equals: " + city1.Equals(city2));
+            Console.WriteLine("City 1 hash code: " + city1.GetHashCode());
+            Console.WriteLine("City 2 hash code: " + city2.GetHashCode() + "\n");
+            Console.WriteLine("City 1: " + city1.ToString());
+            Console.WriteLine("City 2: " + city2.ToString());
         }
     }
 }
