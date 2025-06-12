72143a7f230dedefddedb968f7f69cb0f9d2d002
DmitriyGron <unvmj5105775@gmail.com>
2025-06-12 16:43:49 +0300

    new program

---
+++ HW-004/HW-004-4/CreditCard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HW_004_4
+{
+    internal class CreditCard
+    {
+        public int CVC { get; set; }
+        public decimal Balance { get; set; }
+
+        public CreditCard(int cvc, decimal balance)
+        {
+            CVC = cvc;
+            Balance = balance;
+        }
+
+        public static CreditCard operator +(CreditCard card, decimal amount)
+        {
+            card.Balance += amount;
+            return card;
+        }
+
+        public static CreditCard operator -(CreditCard card, decimal amount)
+        {
+            if (card.Balance < amount)
+                throw new InvalidOperationException("Insufficient funds.");
+            card.Balance -= amount;
+            return card;
+        }
+
+        public static bool operator ==(CreditCard a, CreditCard b) => a.CVC == b.CVC;
+        public static bool operator !=(CreditCard a, CreditCard b) => !(a.CVC == b.CVC);
+        public static bool operator >(CreditCard a, CreditCard b) => a.Balance > b.Balance;
+        public static bool operator <(CreditCard a, CreditCard b) => a.Balance < b.Balance;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is CreditCard other)
+            {
+                return this.CVC == other.CVC && this.Balance == other.Balance;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return CVC.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"CVC: {CVC}, Balance: {Balance}";
+
+        }
+    }
+}
--- HW-004/HW-004-4/Program.cs
+++ HW-004/HW-004-4/Program.cs
@@ -10,6 +10,31 @@ namespace HW_004_4
     {
         static void Main(string[] args)
         {
+            CreditCard card1 = new CreditCard(123, 6000);
+            CreditCard card2 = new CreditCard(456, 3000);
+
+            Console.WriteLine("Card 1: " + card1);
+            Console.WriteLine("Card 2: " + card2 + "\n");
+
+            Console.WriteLine("Adding 2000 to Card 1's balance...");
+            card1 += 2000;
+            Console.WriteLine("Card 1: " + card1 + "\n");
+
+            Console.WriteLine("Subtracting 1500 from Card 2's balance...");
+            card2 -= 1500;
+            Console.WriteLine("Card 2: " + card2 + "\n");
+
+            Console.WriteLine("Card 1 (CVC) == Card 2 (CVC): " + (card1.CVC == card2.CVC));
+            Console.WriteLine("Card 1 (CVC) != Card 2 (CVC): " + (card1.CVC != card2.CVC));
+            Console.WriteLine("Card 1 > Card 2: " + (card1 > card2));
+            Console.WriteLine("Card 1 < Card 2: " + (card1 < card2));
+            Console.WriteLine("Equals (CVC): " + card1.CVC.Equals(card2.CVC));
+            Console.WriteLine("Card 1 hash code: " + card1.GetHashCode());
+            Console.WriteLine("Card 2 hash code: " + card2.GetHashCode() + "\n");
+
+            Console.WriteLine("Card 1: " + card1.ToString());
+            Console.WriteLine("Card 2: " + card2.ToString());
+
         }
     }
 }
