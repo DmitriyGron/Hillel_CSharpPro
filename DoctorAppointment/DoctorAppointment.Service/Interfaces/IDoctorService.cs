0ed260a355d5418a651814ec2cb8a11c555f9105
DmitriyGron <unvmj5105775@gmail.com>
2025-07-23 01:38:53 +0300

    new functionalities

---
+++ DoctorAppointment/DoctorAppointment.Service/Interfaces/IDoctorService.cs
@@ -0,0 +1,18 @@
+﻿using DoctorAppointment.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointment.Service.Interfaces
+{
+    public interface IDoctorService
+    {
+        Doctor Create(Doctor doctor);
+        IEnumerable<Doctor> GetAll();
+        Doctor? Get(int id);
+        bool Delete(int id);
+        Doctor Update(int id, Doctor doctor);
+    }
+}
--- DoctorAppointment/DoctorAppointment/Program.cs
+++ DoctorAppointment/DoctorAppointment/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using DoctorAppointment.Domain.Entities;
+using DoctorAppointment.Service.Interfaces;
+using DoctorAppointment.Service.Services;
 
 namespace DoctorAppointment
 {
-    internal class Program
+    public class DoctorAppointment
     {
-        static void Main(string[] args)
-        {
+        private readonly IDoctorService _doctorService;
 
+        public DoctorAppointment()
+        {
+            _doctorService = new DoctorService();
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Current doctors list: ");
+            var docs = _doctorService.GetAll();
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Name);
+            }
+
+            Console.WriteLine("Adding doctor: ");
+            var newDoctor = new Doctor
+            {
+                Name= "Dr. Smith",
+                Surname = "Petrov",
+                Experience = 11,
+                DoctorType = Domain.Enums.DoctorTypes.Dentist
+            };
+
+            _doctorService.Create(newDoctor);
+            Console.WriteLine("Current doctors list: ");
+            docs = _doctorService.GetAll();
+
+            foreach (var doc in docs)
+            {
+                Console.WriteLine(doc.Name);
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var doctorAppointment = new DoctorAppointment();
+            doctorAppointment.Menu();
         }
     }
 }
