84900bf218174f0eaed7f9c7af65b0a3266b2255
DmitriyGron <unvmj5105775@gmail.com>
2025-07-23 15:46:46 +0300

    first try to add new patient

--- DoctorAppointment/DoctorAppointment.Domain/Entities/Auditable.cs
+++ DoctorAppointment/DoctorAppointment.Domain/Entities/Auditable.cs
@@ -2,6 +2,8 @@
 {
     public abstract class Auditable
     {
+        public int Id { get; set; }
+
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
--- DoctorAppointment/DoctorAppointment/Program.cs
+++ DoctorAppointment/DoctorAppointment/Program.cs
@@ -1,9 +1,53 @@
 ﻿using DoctorAppointment.Domain.Entities;
 using DoctorAppointment.Service.Interfaces;
 using DoctorAppointment.Service.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace DoctorAppointment
 {
+    public class  PatientAppointment
+    {
+        private readonly IPatientService _patientService;
+
+        public PatientAppointment()
+        {
+            _patientService = new PatientService();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Current Patient list: ");
+            var patient = _patientService.GetAll();
+
+            foreach (var pat in patient)
+            {
+                Console.WriteLine(pat.Name);
+            }
+
+            Console.WriteLine("Adding patient: ");
+            var newPatient = new Patient
+            {
+                Name = "Oleg",
+                Surname = "Gydrov",
+                IllnessType = Domain.Enums.IllnessTypes.Ambulance,
+                AddittionalInfo = "No additional info",
+                Address = "Kyiv, Ukraine",
+                Phone = "123-456-7890",
+                Email = "unvmj5105775@mail.com"
+            };
+
+            _patientService.Create(newPatient);
+            Console.WriteLine("Current doctors list: ");
+            patient = _patientService.GetAll();
+
+            foreach (var pat in patient)
+            {
+                Console.WriteLine(pat.Name);
+            }
+        }
+
+    }
+
     public class DoctorAppointment
     {
         private readonly IDoctorService _doctorService;
@@ -15,31 +59,40 @@ namespace DoctorAppointment
 
         public void Menu()
         {
-            Console.WriteLine("Current doctors list: ");
-            var docs = _doctorService.GetAll();
-
-            foreach (var doc in docs)
+            while (true)
             {
-                Console.WriteLine(doc.Name);
+                //add enum for menu items and describe menu
+                
             }
 
-            Console.WriteLine("Adding doctor: ");
-            var newDoctor = new Doctor
-            {
-                Name= "Dr. Smith",
-                Surname = "Petrov",
-                Experience = 11,
-                DoctorType = Domain.Enums.DoctorTypes.Dentist
-            };
+            //Console.WriteLine("Current doctors list: ");
+            //var docs = _doctorService.GetAll();
 
-            _doctorService.Create(newDoctor);
-            Console.WriteLine("Current doctors list: ");
-            docs = _doctorService.GetAll();
+            //foreach (var doc in docs)
+            //{
+            //    Console.WriteLine(doc.Name);
+            //}
 
-            foreach (var doc in docs)
-            {
-                Console.WriteLine(doc.Name);
-            }
+            //Console.WriteLine("Adding doctor: ");
+            //var newDoctor = new Doctor
+            //{
+            //    Name = "Dr. Smith",
+            //    Surname = "Petrov",
+            //    Experience = 11,
+            //    DoctorType = Domain.Enums.DoctorTypes.Dentist
+            //};
+
+            //_doctorService.Create(newDoctor);
+            //Console.WriteLine("Current doctors list: ");
+            //docs = _doctorService.GetAll();
+
+            //foreach (var doc in docs)
+            //{
+            //    Console.WriteLine(doc.Name);
+            //}
+
+
+            
         }
     }
 
@@ -47,8 +100,12 @@ namespace DoctorAppointment
     {
         public static void Main(string[] args)
         {
-            var doctorAppointment = new DoctorAppointment();
-            doctorAppointment.Menu();
+            //var doctorAppointment = new DoctorAppointment();
+            //doctorAppointment.Menu();
+
+            var patientAppointment = new PatientAppointment();
+            patientAppointment.Run();
+
         }
     }
 }
