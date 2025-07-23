532deea45efda1bb87986e6f0f2133c5bc4a0eb4
DmitriyGron <unvmj5105775@gmail.com>
2025-07-23 23:40:36 +0300

    added more IllnessTypes

--- DoctorAppointment/DoctorAppointment.Domain/Enums/IllnessTypes.cs
+++ DoctorAppointment/DoctorAppointment.Domain/Enums/IllnessTypes.cs
@@ -2,10 +2,24 @@
 {
     public enum IllnessTypes
     {
-        EyeDisease = 1,
-        Infection,
-        DentalDisease,
-        SkinDisease,
-        Ambulance,
+        EyeDisease = 1,         
+        Infection,              
+        DentalDisease,          
+        SkinDisease,           
+        Ambulance,              
+        CardiovascularDisease,  
+        NeurologicalDisorder,   
+        RespiratoryDisease,     
+        GastrointestinalDisease,
+        EndocrineDisorder,      
+        MusculoskeletalDisease, 
+        PsychiatricDisorder,    
+        Oncology,               
+        PediatricDisease,       
+        RheumatologicDisease,   
+        AllergicReaction,       
+        MetabolicDisorder,      
+        InfectiousDisease,      
+        TropicalDisease         
     }
 }
