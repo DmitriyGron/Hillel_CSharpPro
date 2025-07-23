174e46b35649945ab0471e794fb438d39db70c2f
DmitriyGron <unvmj5105775@gmail.com>
2025-07-23 23:44:15 +0300

    output tests

--- DoctorAppointment/DoctorAppointment/Program.cs
+++ DoctorAppointment/DoctorAppointment/Program.cs
@@ -1,11 +1,11 @@
-﻿using DoctorAppointment.Domain.Entities;
+﻿using DoctorAppointment.Data.Repositories;
+using DoctorAppointment.Domain.Entities;
 using DoctorAppointment.Service.Interfaces;
 using DoctorAppointment.Service.Services;
-using System.ComponentModel.DataAnnotations;
 
 namespace DoctorAppointment
 {
-    public class  PatientAppointment
+    public class PatientAppointment
     {
         private readonly IPatientService _patientService;
 
@@ -16,34 +16,36 @@ namespace DoctorAppointment
 
         public void Run()
         {
+
             Console.WriteLine("Current Patient list: ");
-            var patient = _patientService.GetAll();
+            var patientRepo = new PatientRepository();
+            var patients = _patientService.GetAll();
 
-            foreach (var pat in patient)
+            foreach (var pat in patients)
             {
-                Console.WriteLine(pat.Name);
+                patientRepo.ShowInfo(pat);
             }
 
-            Console.WriteLine("Adding patient: ");
-            var newPatient = new Patient
-            {
-                Name = "Oleg",
-                Surname = "Gydrov",
-                IllnessType = Domain.Enums.IllnessTypes.Ambulance,
-                AddittionalInfo = "No additional info",
-                Address = "Kyiv, Ukraine",
-                Phone = "123-456-7890",
-                Email = "unvmj5105775@mail.com"
-            };
+            //Console.WriteLine("Adding patient: ");
+            //var newPatient = new Patient
+            //{
+            //    Name = "Oleg",
+            //    Surname = "Gydrov",
+            //    IllnessType = Domain.Enums.IllnessTypes.Ambulance,
+            //    AddittionalInfo = "No additional info",
+            //    Address = "Kyiv, Ukraine",
+            //    Phone = "123-456-7890",
+            //    Email = "unvmj5105775@mail.com"
+            //};
 
-            _patientService.Create(newPatient);
-            Console.WriteLine("Current doctors list: ");
-            patient = _patientService.GetAll();
+            //_patientService.Create(newPatient);
+            //Console.WriteLine("Current doctors list: ");
+            //patients = _patientService.GetAll();
 
-            foreach (var pat in patient)
-            {
-                Console.WriteLine(pat.Name);
-            }
+            //foreach (var pat in patients)
+            //{
+            //    Console.WriteLine(pat.Name);
+            //}
         }
 
     }
@@ -59,19 +61,20 @@ namespace DoctorAppointment
 
         public void Menu()
         {
-            while (true)
+            //while (true)
+            //{
+            //    //add enum for menu items and describe menu
+
+            //}
+            Console.WriteLine("Current doctors list: ");
+            var docs = _doctorService.GetAll();
+            var doctorRepo = new DoctorRepository();
+
+            foreach (var doc in docs)
             {
-                //add enum for menu items and describe menu
-                
+                doctorRepo.ShowInfo(doc);
             }
 
-            //Console.WriteLine("Current doctors list: ");
-            //var docs = _doctorService.GetAll();
-
-            //foreach (var doc in docs)
-            //{
-            //    Console.WriteLine(doc.Name);
-            //}
 
             //Console.WriteLine("Adding doctor: ");
             //var newDoctor = new Doctor
@@ -92,7 +95,7 @@ namespace DoctorAppointment
             //}
 
 
-            
+
         }
     }
 
@@ -100,12 +103,12 @@ namespace DoctorAppointment
     {
         public static void Main(string[] args)
         {
-            //var doctorAppointment = new DoctorAppointment();
-            //doctorAppointment.Menu();
+            var doctorAppointment = new DoctorAppointment();
+            doctorAppointment.Menu();
+
 
             var patientAppointment = new PatientAppointment();
             patientAppointment.Run();
-
         }
     }
 }
